8c74218b670f43ab74dafd6a53c265ffdd2f6bdf
yotamba12 <yotambaram@gmail.com>
2020-07-16 13:45:15 -0700

    start api deelopment

---
+++ utils/API.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+//const BASE_URL = "http://localhost:8080"
+const BASE_URL = "https://gotutorback.herokuapp.com"
+
+const API = {
+    login:function(userData){
+        return axios.post(`${BASE_URL}/login`,userData,{withCredentials:true})
+    },
+    createUser:function(userData){
+        return axios.post(`${BASE_URL}/signup`,userData,{withCredentials:true})
+    },
+  
+
+
+}
+export default API
+
